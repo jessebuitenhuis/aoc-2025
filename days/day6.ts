399c6c62ad2e3d7fdaebfc0ab7ffed5532081801
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2026-01-05 16:37:32 +0100

    day6

--- days/day6.ts
+++ days/day6.ts
@@ -1,16 +1,10 @@
-import {
-  algebra,
-  multiply,
-  readAllLines,
-  readLines,
-  sum,
-  sumBy,
-  toNumber,
-} from "../utils";
+import { algebra, readAllLines, sumBy, toNumber } from "../utils";
 
 const { example, input } = await readAllLines(6);
 solveA(example);
 solveA(input);
+solveB(example);
+solveB(input);
 
 function solveA(input: string[]) {
   const problems = parseInput(input);
@@ -18,15 +12,47 @@ function solveA(input: string[]) {
   console.log(sum);
 }
 
+function solveB(input: string[]) {
+  let sum = 0;
+  for (let problem of getProblems(input)) {
+    sum += algebra(problem.operator, problem.digits);
+  }
+
+  console.log(sum);
+}
+
 function solveProblem(input: string[]) {
   const operator = input.pop();
   const digits = input.map(toNumber);
 
-  if (!operator) throw new Error("Operator not found.");
+  if (!operator) throw new Error(`Operator ${operator} not found.`);
   return algebra(operator, digits);
 }
 
 function parseInput(input: string[]) {
   const parsed = input.map((line) => line.trim().split(/\s+/));
   return parsed[0].map((_, i) => parsed.map((y) => y[i]));
+}
+
+function* getProblems(input: string[]) {
+  let digits: number[] = [];
+  let operator = "";
+  let x = 0;
+
+  while (true) {
+    const chars = input.map((line) => line[x]);
+    const operatorChar = chars.pop() ?? " ";
+    if (operatorChar !== " ") operator = operatorChar;
+    const digitChars = chars.join("").trim();
+    if (digitChars.length === 0) {
+      yield { digits, operator };
+      digits = [];
+    } else {
+      const digit = toNumber(digitChars);
+      digits.push(digit);
+    }
+
+    if (chars.every((x) => x === undefined)) break;
+    x++;
+  }
 }
