f2657c393a89582463a96279509888f2fe73f1e9
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2026-01-05 12:20:27 +0100

    day3b

--- days/day3.ts
+++ days/day3.ts
@@ -1,26 +1,43 @@
 import { readLines, sum, toNumbers } from "../utils";
 
-function findLargestJoltage(bank: string) {
+function findLargestJoltage(bank: string, count = 2) {
   const numbers = toNumbers(bank);
 
-  const largest = Math.max(...numbers.slice(0, -1));
-  const largestIndex = numbers.indexOf(largest);
+  let start = 0;
+  let output = "";
 
-  const largest2 = Math.max(...numbers.slice(largestIndex + 1));
+  for (let i = 0; i < count; i++) {
+    const end = -1 * count + i + 1;
+    const slice = numbers.slice(start, end === 0 ? undefined : end);
+    const largest = Math.max(...slice);
+    const largestIndex = slice.indexOf(largest);
 
-  return parseInt(`${largest}${largest2}`);
+    output += largest;
+    start = start + largestIndex + 1;
+  }
+
+  return parseInt(output);
 }
 
 function solveA(input: string[]) {
-  const joltages = input.map(findLargestJoltage);
+  const joltages = input.map((x) => findLargestJoltage(x));
   return sum(joltages);
 }
 
-const exampleA = await readLines("example3");
-const answerExampleA = solveA(exampleA);
+function solveB(input: string[]) {
+  const joltages = input.map((x) => findLargestJoltage(x, 12));
+  return sum(joltages);
+}
 
-const inputA = await readLines("input3");
-const answerA = solveA(inputA);
+const example = await readLines("example3");
+const answerExampleA = solveA(example);
+const answerExampleB = solveB(example);
+
+const input = await readLines("input3");
+const answerA = solveA(input);
+const answerB = solveB(input);
 
 console.log("Example A:", answerExampleA);
+console.log("Example B:", answerExampleB);
 console.log("A:", answerA);
+console.log("B:", answerB);
